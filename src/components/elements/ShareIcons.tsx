55f9b1aa76df15d095745408377eef522aca32e0
kahboom <rachelyordan@gmail.com>
2022-06-08 20:57:13 +0100

    add social urls

--- src/components/elements/ShareIcons.tsx
+++ src/components/elements/ShareIcons.tsx
@@ -1,46 +1,34 @@
 export interface IShareIcons {
-  to?: string;
+  image?: string;
+  title?: string;
   url?: string;
 }
 
-const ShareIcons = ({ to, url }: IShareIcons) => {
+const ShareIcons = ({ image, title, url }: IShareIcons) => {
   return (
-    <div className='social-icons single-share'>
-      <ul className='text-grey-5 d-inline-block'>
+    <div className="social-icons single-share">
+      <ul className="text-grey-5 d-inline-block">
         <li>
-          <strong className='mr-10'>Share this:</strong>
+          <strong className="mr-10">Share this:</strong>
         </li>
-        <li className='social-facebook'>
-          <a href='src/components/elements/ShareIcons#'>
-            <img
-              src='/assets/images/theme/icons/icon-facebook.svg'
-              alt='Facebook'
-            />
+        <li className="social-facebook">
+          <a href={`https://www.facebook.com/sharer.php?u=${url}`}>
+            <img src="/assets/images/theme/icons/icon-facebook.svg" alt="Facebook" />
           </a>
         </li>
-        <li className='social-twitter'>
-
-          <a href='src/components/elements/ShareIcons#'>
-            <img
-              src='/assets/images/theme/icons/icon-twitter.svg'
-              alt='Twitter'
-            />
+        <li className="social-twitter">
+          <a href={`https://twitter.com/intent/tweet?url=${url}&text=${title}`}>
+            <img src="/assets/images/theme/icons/icon-twitter.svg" alt="Twitter" />
           </a>
         </li>
-        <li className='social-instagram'>
-          <a href='src/components/elements/ShareIcons#'>
-            <img
-              src='/assets/images/theme/icons/icon-instagram.svg'
-              alt='Instagram'
-            />
+        <li className="social-instagram">
+          <a href={`https://www.instagram.com/?url=${url}`}>
+            <img src="/assets/images/theme/icons/icon-instagram.svg" alt="Instagram" />
           </a>
         </li>
-        <li className='social-linkedin'>
-          <a href='src/components/elements/ShareIcons#'>
-            <img
-              src='/assets/images/theme/icons/icon-pinterest.svg'
-              alt='Pinterest'
-            />
+        <li className="social-linkedin">
+          <a href={`https://pinterest.com/pin/create/button/?url=${url}`}>
+            <img src="/assets/images/theme/icons/icon-pinterest.svg" alt="Pinterest" />
           </a>
         </li>
       </ul>
