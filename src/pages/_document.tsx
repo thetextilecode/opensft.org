849347af369629bf7441d85b59aa406fb9bff6dd
kahboom <rachelyordan@gmail.com>
2022-07-13 19:43:17 +0100

    rm tag manager, add universal

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,35 +1,33 @@
 import Document, { Head, Html, Main, NextScript } from 'next/document';
 import Script from 'next/script';
-import { seoConfig } from '../../opensft.config';
 
 class MyDocument extends Document {
   render(): JSX.Element {
     return (
       <Html lang="en">
         <Head>
-          {/*<!-- Google Tag Manager -->*/}
+          {/*<!-- Global site tag (gtag.js) - Google Analytics -->*/}
           <Script
             strategy="afterInteractive"
-            id={'tag-manager'}
+            async
+            src={'https://www.googletagmanager.com/gtag/js?id=G-9WS3JV03MY'}
+            id={'site-tag-01'}
+          ></Script>
+          <Script
+            strategy="afterInteractive"
+            id={'site-tag-02'}
             dangerouslySetInnerHTML={{
-              __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-        })(window,document,'script','dataLayer','${seoConfig.googleTagManager}');`,
+              __html: `window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-9WS3JV03MY');`,
             }}
           ></Script>
-          {/*<!-- End Google Tag Manager -->*/}
         </Head>
         <body>
           <Main />
           <NextScript />
-          <noscript
-            dangerouslySetInnerHTML={{
-              __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=${seoConfig.googleTagManager}"
-height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
-            }}
-          ></noscript>
         </body>
       </Html>
     );
