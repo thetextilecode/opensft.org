cda13a8417aa8a9d24d6f58528069da63b422e08
kahboom <rachelyordan@gmail.com>
2023-02-12 11:28:23 +0000

    cleanup

--- src/pages/resources/index.tsx
+++ src/pages/resources/index.tsx
@@ -99,33 +99,6 @@ const Resources = ({
                 <h5 className="section-title style-1 mb-30 wow fadeIn animated">Category</h5>
                 <CategoryResource />
               </div>
-
-              {/*<div className="sidebar-widget product-sidebar  mb-30 p-30 bg-grey border-radius-10">*/}
-              {/*  <div className="widget-header position-relative mb-20 pb-10">*/}
-              {/*    <h5 className="widget-title mb-10">New Resources</h5>*/}
-              {/*    <div className="bt-1 border-color-1"></div>*/}
-              {/*  </div>*/}
-              {/*  <div className="single-post clearfix">*/}
-              {/*    <div className="image">*/}
-              {/*      <Image*/}
-              {/*        src="/assets/images/page/placeholder.png"*/}
-              {/*        alt="#"*/}
-              {/*        layout={'responsive'}*/}
-              {/*        width={600}*/}
-              {/*        height={600}*/}
-              {/*      />*/}
-              {/*    </div>*/}
-              {/*    <div className="content pt-10">*/}
-              {/*      <h6>*/}
-              {/*        <a>Example Project</a>*/}
-              {/*      </h6>*/}
-              {/*      <p className="price mb-0 mt-5">CRM</p>*/}
-              {/*      <div className="product-rate">*/}
-              {/*        <div className="product-rating" style={{ width: '60%' }}></div>*/}
-              {/*      </div>*/}
-              {/*    </div>*/}
-              {/*  </div>*/}
-              {/*</div>*/}
             </div>
             <div className="col-lg-9">
               <div className="shop-product-filter">
